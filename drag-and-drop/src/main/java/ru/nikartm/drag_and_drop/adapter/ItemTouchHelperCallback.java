e6e1e0dda3e421b0999799aaa42df0eed4c5684e
Ivan Vodyasov <ivodyasov@at-consulting.ru>
2017-07-09 22:55:20 +0300

    Added swipe animation

--- drag-and-drop/src/main/java/ru/nikartm/drag_and_drop/adapter/ItemTouchHelperCallback.java
+++ drag-and-drop/src/main/java/ru/nikartm/drag_and_drop/adapter/ItemTouchHelperCallback.java
@@ -1,5 +1,6 @@
 package ru.nikartm.drag_and_drop.adapter;
 
+import android.graphics.Canvas;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.helper.ItemTouchHelper;
 
@@ -41,5 +42,18 @@ public class ItemTouchHelperCallback extends ItemTouchHelper.Callback {
     @Override
     public boolean isItemViewSwipeEnabled() {
         return true;
+    }
+
+    @Override
+    public void onChildDraw(Canvas c, RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder,
+                            float dX, float dY, int actionState, boolean isCurrentlyActive) {
+        // Fade out the view if swiped item
+        if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+            final float alpha = 1.0f - Math.abs(dX) / (float) viewHolder.itemView.getWidth();
+            viewHolder.itemView.setAlpha(alpha);
+            viewHolder.itemView.setTranslationX(dX);
+        } else {
+            super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+        }
     }
 }
