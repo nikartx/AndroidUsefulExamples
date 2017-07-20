aaa3812bf6f194fb89f45351a2ed6082c21f04d7
Ivan Vodyasov <ivodyasov@at-consulting.ru>
2017-07-20 19:53:07 +0300

    Create project

---
+++ carousel-viewpager/src/main/java/ru/nikartm/carousel_viewpager/MainActivity.java
@@ -0,0 +1,13 @@
+package ru.nikartm.carousel_viewpager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
